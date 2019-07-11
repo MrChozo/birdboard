d58fddc5c0ecefa28d3d454c1d6e73adf162cfba
Brian Weston <brianw@webriculture.com>
2019-07-11 12:01:10 -0700

    Add a_user_can_view_a_project feature test

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Project</title>
+</head>
+<body>
+    <h1>{{ $project->title }}</h1>
+    <div>{{ $project->description }}</div>
+</body>
+</html>
