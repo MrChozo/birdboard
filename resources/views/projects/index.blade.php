76a90e60b4aa46962a36fa03fcdd56e82b2508d0
Brian Weston <brianw@webriculture.com>
2019-08-03 10:49:37 -0700

    Finish up lesson 10, styling dashboard

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,11 +9,13 @@
         </div>
     </header>
 
-    <main class="flex flex-wrap -mx-3">
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="w-1/3 px-3 pb-6">
-                <div class="bg-white rounded shadow p-5" style="height: 200px;">
-                    <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-lg shadow p-5" style="height: 200px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-light pl-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h3>
                     <div class="text-grey">{{ Str::limit($project->description, 100, "...") }}</div>
                 </div>
             </div>
