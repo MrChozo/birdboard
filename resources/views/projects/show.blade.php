649317a35ae86df16b311df7157e47760352d5fb
Brian Weston <brianw@webriculture.com>
2019-08-04 13:57:01 -0700

    Finish lesson 11, styling projects.show

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $project->title }}</h1>
-    <div>{{ $project->description }}</div>
-    <div><a href="/projects">Go Back</a></div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-grey text-sm font-normal">
+                <a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+
+            <a href="/projects/create" class="button">Add Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="lg:flex -mx-3">
+            <div class="lg:w-3/4 mb-6 px-3">
+                <div class="mb-8">
+                    <h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+                    {{-- tasks --}}
+
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div class="card">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                </div>
+                <div>
+                    <h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+
+                    {{-- general notes --}}
+                    <textarea class="card w-full" style="min-height: 200px;">Lorem ipsum dolor sit amet, consectetu</textarea>
+                </div>
+            </div>
+            <div class="lg:w-1/4 px-3">
+                @include('projects.card')
+            </div>
+        </div>
+    </main>
 @endsection
