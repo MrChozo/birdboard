4c7c14a8d267909ba75a49c78c26ec09f83ef1f0
Brian Weston <brianw@webriculture.com>
2019-11-21 15:48:16 -0800

    More Scheduled Task CI/CD cheese

--- app/Task.php
+++ app/Task.php
@@ -9,4 +9,5 @@ class Task extends Model
     protected $guarded = [];
     
     // Not at all related to this project, I'm going to test a Windows Scheduled Task on this repo
+    // Seriously, the task will be scheduled this time. Had to test a Powershell script first.
 }
