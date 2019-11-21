027cf7a6618ea8c4770bfa2a79f22620f5cc0b0f
Brian Weston <brianw@webriculture.com>
2019-11-21 15:29:40 -0800

    Test cheese-ball Windows Scheduled Task CI/CD

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
+    
+    // Not at all related to this project, I'm going to test a Windows Scheduled Task on this repo
 }
