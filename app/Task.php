23a46931b139bc78ef25852b512b7c0d20ebdd8d
Brian Weston <brianw@webriculture.com>
2019-11-21 16:32:47 -0800

    It's the cheesiest. -Scheduled Tasks

--- app/Task.php
+++ app/Task.php
@@ -10,4 +10,5 @@ class Task extends Model
     
     // Not at all related to this project, I'm going to test a Windows Scheduled Task on this repo
     // Seriously, the task will be scheduled this time. Had to test a Powershell script first.
+    // It ran, but only when I ran it manually. Windows has some UI problems in this area.
 }
